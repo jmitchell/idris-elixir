d55c8699c6041ad3d3c08e13c8967cb829633290
Jacob Mitchell <jmitchell@member.fsf.org>
2017-01-19 20:03:46 -0800

    "Hello world" working with Elixir interpreter.
    To make quicker initial progress I took the PHP code generator and adapted as
    needed to get "Hello world" running. There are still parts emitting PHP, and
    should be patched soon. There are also several unimplemented parts (see fallback
    cases of functions).

--- test/test.sh
+++ test/test.sh
@@ -7,14 +7,11 @@ TEST="${TEST_INPUT_DIR}/$1"
 TMP_DIR="./tmp"
 TMP_FILE_PREFIX="${TMP_DIR}/$(basename ${TEST})"
 
+rm -rf "${TMP_DIR}"
 mkdir -p "${TMP_DIR}"
+
 idris "${TEST}.idr" -o "${TMP_FILE_PREFIX}"
 mv "${TEST}.ibc" "${TMP_FILE_PREFIX}.ibc"
 stack build
-./.stack-work/install/x86_64-linux/lts-7.16/8.0.1/bin/idris-elixir "${TMP_FILE_PREFIX}.ibc" -o "${TMP_FILE_PREFIX}.ex"
-
-echo ""
-echo "Generated output"
-echo "================"
-cat "${TMP_FILE_PREFIX}.ex"
-rm -rf "${TMP_DIR}"
+./.stack-work/install/x86_64-linux/lts-7.16/8.0.1/bin/idris-elixir "${TMP_FILE_PREFIX}.ibc" -o "${TMP_FILE_PREFIX}.exs"
+elixir "${TMP_FILE_PREFIX}.exs"
